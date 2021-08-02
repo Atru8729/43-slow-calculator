01dde747cc14b371bb1d1d97af6f531ed5b7008d
Atru8729 <a.truskauskas1@gmail.com>
2021-08-02 20:08:36 +0300

    final

--- calculator.js
+++ calculator.js
@@ -1,4 +1,13 @@
-async function calculator() {
+async function calculator(a, b) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof a === 'number' && typeof b === 'number') {
+                resolve(a + b);
+            } else {
+                reject('Bad sum');
+            }
+        }, 500);
+    })
 }
 
 module.exports = calculator;
